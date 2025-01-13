0fa8480869ef5a2506812bfbaddd192e5b4d2898
pirjademubin <pirjademubinn@gmail.com>
2025-01-13 22:10:32 +0530

    Updated Files

---
+++ avactisautomation/src/test/java/com/avactis/pages/SignInPage.java
@@ -0,0 +1,46 @@
+package com.avactis.pages;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SignInPage extends BaseClass {
+	WebDriver driver;
+	
+	public SignInPage(WebDriver ldriver) {
+		this.driver = ldriver;		
+	}
+	
+	@FindBy(xpath = "//a[text()='Sign In']")
+	WebElement signIn;
+	
+	@FindBy(xpath = "//input[@id='account_sign_in_form_email_id']")
+	WebElement userName;
+	
+	@FindBy(xpath = "//input[@id='account_sign_in_form_passwd_id']")
+	WebElement userPass;
+	
+	@FindBy(xpath = "//input[@value='Sign In']")
+	WebElement signInButton;
+	
+	public void signIn(String uname, String pass) throws InterruptedException {
+		signIn.click();
+		userName.sendKeys(uname);
+		userPass.sendKeys(pass);
+		signInButton.click();
+		Thread.sleep(2000);
+		String ExpectedPageHeader = "MANAGE ACCOUNT AND VIEW ORDERS";
+		String ActualPageHeader = null;
+		try {
+			ActualPageHeader = driver.findElement(By.xpath("//h3[text()='Manage Account and View Orders']")).getText();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			logger.info("Wrong Xpath" + e.getMessage());
+			//e.printStackTrace();
+		}
+		assertEquals(ExpectedPageHeader, ActualPageHeader );
+	}
+}
--- avactisautomation/src/test/java/com/avactis/testcases/RegisterUserTest.java
+++ avactisautomation/src/test/java/com/avactis/testcases/RegisterUserTest.java
@@ -5,80 +5,109 @@ import org.testng.ITestResult;
 import org.testng.annotations.Test;
 
 import com.avactis.pages.BaseClass;
+import com.avactis.pages.SignInPage;
 import com.avactis.pages.registerPage;
 import com.avactis.utilities.ExcelDataProvider;
 
 public class RegisterUserTest extends BaseClass {
 	
+//	@Test(priority=1)
+//	public void RegisterUser() throws InterruptedException {
+//		ExcelDataProvider excel = new ExcelDataProvider();
+//		logger = extent.createTest("Avactis Positive Test");
+//		logger.info("Avactis Test Has been created successfully");
+//		registerPage objregisterPage = PageFactory.initElements(driver, registerPage.class);
+//		logger.info("Page Factory has been initialised");
+//		logger.info("Starting registration process...");
+//		objregisterPage.registrationProcess(
+//				excel.getStringData("RegistrationDetails", 0, 0),
+//				excel.getStringData("RegistrationDetails", 0, 1),
+//				excel.getStringData("RegistrationDetails", 0, 2),
+//				excel.getStringData("RegistrationDetails", 0, 3),
+//				excel.getStringData("RegistrationDetails", 0, 4),
+//				excel.getStringData("RegistrationDetails", 0, 5),
+//				excel.getNumericData("RegistrationDetails", 0, 6),
+//				excel.getStringData("RegistrationDetails", 0, 7),
+//				excel.getStringData("RegistrationDetails", 0, 8),
+//				excel.getStringData("RegistrationDetails", 0, 9),
+//				excel.getNumericData("RegistrationDetails", 0, 10)
+//				);
+//		logger.info("Registration Function executed successfully");
+//		logger.info("Checking Registartion Status...");
+//		if(objregisterPage.checkRegistrationStatus() == 1) {
+//			logger.fail("Sorry, user is already registered !!");
+//			result.setStatus(ITestResult.FAILURE);
+//		}
+//		else if(objregisterPage.checkRegistrationStatus() == 2) {
+//			logger.pass("New user has been registered successfully !!!");
+//			result.setStatus(ITestResult.SUCCESS);
+//		}
+//		logger.info("Registration Status Check completed");
+//	}
 	
+//	@Test(priority=2)
+//	public void RegisterSameUserAgain() throws InterruptedException {
+//		ExcelDataProvider excel = new ExcelDataProvider();
+//		logger = extent.createTest("Avactis Negative Test");
+//		logger.info("Avactis Test Has been created successfully");
+//		registerPage objregisterPage = PageFactory.initElements(driver, registerPage.class);
+//		logger.info("Page Factory has been initialised");
+//		logger.info("Starting registration process...");
+//		objregisterPage.registrationProcess(
+//				excel.getStringData("RegistrationDetails", 0, 0),
+//				excel.getStringData("RegistrationDetails", 0, 1),
+//				excel.getStringData("RegistrationDetails", 0, 2),
+//				excel.getStringData("RegistrationDetails", 0, 3),
+//				excel.getStringData("RegistrationDetails", 0, 4),
+//				excel.getStringData("RegistrationDetails", 0, 5),
+//				excel.getNumericData("RegistrationDetails", 0, 6),
+//				excel.getStringData("RegistrationDetails", 0, 7),
+//				excel.getStringData("RegistrationDetails", 0, 8),
+//				excel.getStringData("RegistrationDetails", 0, 9),
+//				excel.getNumericData("RegistrationDetails", 0, 10)
+//				);
+//		logger.info("Registration Function executed successfully");
+//		logger.info("Checking Registartion Status...");
+//		if(objregisterPage.checkRegistrationStatus() == 1) {
+//			logger.fail("Sorry, user is already registered !!");
+//			result.setStatus(ITestResult.FAILURE);
+//		}
+//		else if(objregisterPage.checkRegistrationStatus() == 2) {
+//			logger.pass("New user has been registered successfully !!!");
+//			result.setStatus(ITestResult.SUCCESS);
+//		}
+//		logger.info("Registration Status Check completed");
+//	}
 	
-//	ExcelDataProvider excel = new ExcelDataProvider();
-//	registerPage objregisterPage = PageFactory.initElements(driver, registerPage.class);
-	@Test(priority=1)
-	public void RegisterUser() throws InterruptedException {
+	@Test(priority = 1)
+	public void SignInUserPositiveTest() throws InterruptedException {
 		ExcelDataProvider excel = new ExcelDataProvider();
-		logger = extent.createTest("Avactis Positive Test");
+		logger = extent.createTest("Avactis SignIn Positive Test");
 		logger.info("Avactis Test Has been created successfully");
-		registerPage objregisterPage = PageFactory.initElements(driver, registerPage.class);
+		SignInPage objsignInPage = PageFactory.initElements(driver, SignInPage.class);
 		logger.info("Page Factory has been initialised");
-		logger.info("Starting registration process...");
-		objregisterPage.registrationProcess(
+		logger.info("Starting SignIn process...");
+		objsignInPage.signIn(
 				excel.getStringData("RegistrationDetails", 0, 0),
-				excel.getStringData("RegistrationDetails", 0, 1),
-				excel.getStringData("RegistrationDetails", 0, 2),
-				excel.getStringData("RegistrationDetails", 0, 3),
-				excel.getStringData("RegistrationDetails", 0, 4),
-				excel.getStringData("RegistrationDetails", 0, 5),
-				excel.getNumericData("RegistrationDetails", 0, 6),
-				excel.getStringData("RegistrationDetails", 0, 7),
-				excel.getStringData("RegistrationDetails", 0, 8),
-				excel.getStringData("RegistrationDetails", 0, 9),
-				excel.getNumericData("RegistrationDetails", 0, 10)
-				);
-		logger.info("Registration Function executed successfully");
-		logger.info("Checking Registartion Status...");
-		if(objregisterPage.checkRegistrationStatus() == 1) {
-			logger.fail("Sorry, user is already registered !!");
-			result.setStatus(ITestResult.FAILURE);
-		}
-		else if(objregisterPage.checkRegistrationStatus() == 2) {
-			logger.pass("New user has been registered successfully !!!");
-			result.setStatus(ITestResult.SUCCESS);
-		}
-		logger.info("Registration Status Check completed");
+				excel.getStringData("RegistrationDetails", 0, 1));
+		logger.info("SignIn Function executed successfully");
+		logger.info("Checking SignIn Status...");
+		logger.pass("Successfully Signed In");
 	}
 	
-	@Test(priority=2)
-	public void RegisterSameUserAgain() throws InterruptedException {
+	@Test(priority = 2)
+	public void SignInUserNegativeTest() throws InterruptedException {
 		ExcelDataProvider excel = new ExcelDataProvider();
-		logger = extent.createTest("Avactis Negative Test");
+		logger = extent.createTest("Avactis SignIn Negative Test");
 		logger.info("Avactis Test Has been created successfully");
-		registerPage objregisterPage = PageFactory.initElements(driver, registerPage.class);
+		SignInPage objsignInPage = PageFactory.initElements(driver, SignInPage.class);
 		logger.info("Page Factory has been initialised");
-		logger.info("Starting registration process...");
-		objregisterPage.registrationProcess(
+		logger.info("Starting SignIn process...");
+		objsignInPage.signIn(
 				excel.getStringData("RegistrationDetails", 0, 0),
-				excel.getStringData("RegistrationDetails", 0, 1),
-				excel.getStringData("RegistrationDetails", 0, 2),
-				excel.getStringData("RegistrationDetails", 0, 3),
-				excel.getStringData("RegistrationDetails", 0, 4),
-				excel.getStringData("RegistrationDetails", 0, 5),
-				excel.getNumericData("RegistrationDetails", 0, 6),
-				excel.getStringData("RegistrationDetails", 0, 7),
-				excel.getStringData("RegistrationDetails", 0, 8),
-				excel.getStringData("RegistrationDetails", 0, 9),
-				excel.getNumericData("RegistrationDetails", 0, 10)
-				);
-		logger.info("Registration Function executed successfully");
-		logger.info("Checking Registartion Status...");
-		if(objregisterPage.checkRegistrationStatus() == 1) {
-			logger.fail("Sorry, user is already registered !!");
-			result.setStatus(ITestResult.FAILURE);
-		}
-		else if(objregisterPage.checkRegistrationStatus() == 2) {
-			logger.pass("New user has been registered successfully !!!");
-			result.setStatus(ITestResult.SUCCESS);
-		}
-		logger.info("Registration Status Check completed");
+				excel.getStringData("RegistrationDetails", 1, 0));
+		logger.info("SignIn Function executed successfully");
+		logger.info("Checking SignIn Status...");
+		logger.fail("Wrong Credentials");
 	}
 }
