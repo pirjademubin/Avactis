325ced7e0b21a683aefe236e439529563b2e1857
pirjademubin <pirjademubinn@gmail.com>
2025-01-16 00:19:33 +0530

    Updated Checkout page

--- avactisautomation/src/test/java/com/avactis/pages/CheckoutPage.java
+++ avactisautomation/src/test/java/com/avactis/pages/CheckoutPage.java
@@ -1,14 +1,18 @@
 package com.avactis.pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 public class CheckoutPage extends BaseClass {
 	WebDriver driver;
-	
 	
 	public CheckoutPage(WebDriver ldriver) {
 		this.driver = ldriver;
@@ -27,6 +31,59 @@ public class CheckoutPage extends BaseClass {
 	@FindBy(linkText = "//select[@name='quantity_in_cart']")
 	WebElement quantity;
 	
+	@FindBy(xpath = "//div[@class=' top-cart-info cart_preview cartpreview']//a[@class='top-cart-info-count']")
+	WebElement cartView;
+	
+	@FindBy(xpath = "//div[@class='top-cart-content']//a[text()='Checkout']")
+	WebElement checkoutButton;
+	
+	@FindBy(xpath = "//input[@name='billingInfo[Firstname]']")
+	WebElement billingInfoFirstName;
+	
+	@FindBy(xpath = "//input[@name='billingInfo[Lastname]']")
+	WebElement billingInfoLastName;
+	
+	@FindBy(xpath = "//input[@name='billingInfo[Email]']")
+	WebElement billingInfoEmail;
+	
+	@FindBy(xpath = "//select[@name='billingInfo[Country]']")
+	WebElement billingInfoCountry;
+	
+	@FindBy(xpath = "//input[@name='billingInfo[Postcode]']")
+	WebElement billingInfoZipCode;
+	
+	@FindBy(xpath = "//select[@name='billingInfo[Statemenu]']")
+	WebElement billingInfoState;
+	
+	@FindBy(xpath = "//input[@name='billingInfo[City]']")
+	WebElement billingInfoCity;
+	
+	@FindBy(xpath = "//input[@name='billingInfo[Streetline1]']")
+	WebElement billingInfoAline1;
+	
+	@FindBy(xpath = "//input[@name='billingInfo[Streetline2]']")
+	WebElement billingInfoAline2;
+	
+	@FindBy(xpath = "//input[@name='billingInfo[Phone]']")
+	WebElement billingInfoPhone;
+	
+	@FindBy(xpath = "//input[@name='checkbox_shipping_same_as_billing']")
+	WebElement billingInfoCheckbox;
+	
+	@FindBy(xpath = "//input[@onclick='submitStep(1);']")
+	WebElement continueCheckOut1;
+	
+	@FindBy(xpath = "//input[@onclick='submitStep(2);']")
+	WebElement continueCheckOut2;
+	
+	@FindBy(xpath = "//div[@class='shipping_form col-lg-6']//label[2]")
+	WebElement verifyAline1;
+	
+	@FindBy(xpath = "//label[text()=' Ground Shipping']//input[1]")
+	WebElement shippingMethod;
+	
+	@FindBy(xpath = "//input[@value='Place Order']")
+	WebElement placeOrderButton;
 	
 	public void SearchProductsandCheckout(String productName) {
 		searchIcon.click();
@@ -37,5 +94,65 @@ public class CheckoutPage extends BaseClass {
 	public double selectQuantity(double uquantity) {
 		quantity.sendKeys(String.valueOf(uquantity));
 		return uquantity;
+	}
+	
+	public void mouseHoverOverCart() {
+		//Instantiating Actions class
+		Actions actions = new Actions(driver);
+
+		//Hovering on main menu
+		actions.moveToElement(cartView).perform();
+	}
+	
+	public void checkoutItems(
+			String fname, 
+			String lname, 
+			String email,
+			String ucountry,
+			double zipcode,
+			String ustate,
+			String city,
+			String aline1,
+			String aline2,
+			double phone
+			)throws InterruptedException {
+		checkoutButton.click();
+		Thread.sleep(2000);
+		billingInfoFirstName.sendKeys("fname");
+		billingInfoLastName.sendKeys(lname);
+		billingInfoEmail.sendKeys(email);
+		
+		Select country = new Select(billingInfoCountry);
+		country.selectByVisibleText(ucountry);
+		
+		billingInfoZipCode.sendKeys(String.valueOf(zipcode));
+		
+		Select state = new Select(billingInfoState);
+		state.selectByVisibleText(ustate);
+		
+		billingInfoCity.sendKeys(city);
+		billingInfoAline1.sendKeys(aline1);
+		billingInfoAline2.sendKeys(aline2);
+		billingInfoPhone.sendKeys(String.valueOf(phone));
+		billingInfoCheckbox.click();
+		logger.info("Continueing checkout after filling billing details...");
+		continueCheckOut1.click();
+		Thread.sleep(5000);
+		
+		logger.info("Verifying Shipping Address...");
+		String aline1verify = verifyAline1.getText();
+		Assert.assertEquals(aline1verify, aline1);
+		if(aline1verify.equals(aline1)) {
+			System.out.println(aline1verify + " is matching with "+ aline1);
+			logger.pass("Shipping Address is correct !!");
+		}
+		
+		
+		shippingMethod.click();
+		continueCheckOut2.click();
+		Thread.sleep(5000);
+		
+		placeOrderButton.click();
+		Thread.sleep(5000);
 	}
 }
--- avactisautomation/src/test/java/com/avactis/testcases/CheckOutTest.java
+++ avactisautomation/src/test/java/com/avactis/testcases/CheckOutTest.java
@@ -1,10 +1,16 @@
 package com.avactis.testcases;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.avactis.pages.BaseClass;
@@ -12,32 +18,94 @@ import com.avactis.pages.CheckoutPage;
 import com.avactis.utilities.ExcelDataProvider;
 
 public class CheckOutTest extends BaseClass {
-	
-	//WebDriverWait wait;
-	
+
+	// WebDriverWait wait;
+
+//	@Test(priority = 1)
+//	public void checkoutProduct1() throws InterruptedException {
+//		ExcelDataProvider excel = new ExcelDataProvider();
+//		logger = extent.createTest("Avactis Positive Test");
+//		logger.info("Avactis Test Has been created successfully");
+//		CheckoutPage objcheckoutpage = PageFactory.initElements(driver, CheckoutPage.class);
+//		logger.info("Page Factory has been initialised");
+//		logger.info("Starting registration process...");
+//		objcheckoutpage.SearchProductsandCheckout(
+//				excel.getStringData("ProductList", 2, 1));
+//		String lText = excel.getStringData("ProductList", 2, 1);
+//		System.out.println(lText);
+//		Thread.sleep(5000);
+//		String xpath = "//a//h3[text()='"+ lText + "']";
+//		driver.findElement(By.xpath(xpath)).click();
+//		Thread.sleep(5000);
+//		driver.findElement(By.xpath("//input[@value='Add To Cart']")).click();
+//		Thread.sleep(5000);
+//		
+//	}
+
 	@Test(priority = 1)
-	public void checkoutProducts() throws InterruptedException {
+	public void checkoutProduct2() throws InterruptedException {
 		ExcelDataProvider excel = new ExcelDataProvider();
 		logger = extent.createTest("Avactis Positive Test");
 		logger.info("Avactis Test Has been created successfully");
 		CheckoutPage objcheckoutpage = PageFactory.initElements(driver, CheckoutPage.class);
 		logger.info("Page Factory has been initialised");
 		logger.info("Starting registration process...");
-		objcheckoutpage.SearchProductsandCheckout(
-				excel.getStringData("ProductList", 2, 1));
-		String lText = excel.getStringData("ProductList", 2, 1);
-		System.out.println(lText);
-		Thread.sleep(5000);
-		String xpath = "//a//h3[text()='"+ lText + "']";
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("driver.findElement(By.linkText(excel.getStringData(\"ProductList\", 2, 1)))")));
-		driver.findElement(By.xpath(xpath)).click();
-		Thread.sleep(5000);
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@name='quantity_in_cart']")));
-//		Select quantity = new Select(driver.findElement(By.xpath("//select[@name='quantity_in_cart']")));
-//		quantity.selectByVisibleText(String.valueOf(objcheckoutpage.selectQuantity(excel.getNumericData("ProductList", 2, 2))));
-//		Thread.sleep(5000);
-		driver.findElement(By.xpath("//input[@value='Add To Cart']")).click();
-		Thread.sleep(5000);
+		List<Boolean> booleanList = new ArrayList<>();
+		for (int i = 2; i < 5; i++) {
+			objcheckoutpage.SearchProductsandCheckout(excel.getStringData("ProductList", i, 1));
+			String lText = excel.getStringData("ProductList", i, 1);
+			System.out.println(lText);
+			Thread.sleep(5000);
+			String xpath = "//a//h3[text()='" + lText + "']";
+			driver.findElement(By.xpath(xpath)).click();
+			Thread.sleep(5000);
+			driver.findElement(By.xpath("//input[@value='Add To Cart']")).click();
+			Thread.sleep(5000);
+		}
+
+//		// Locating the Main Menu (Parent element)
+//		WebElement mainMenu = driver.findElement(By.xpath("//div[@class=' top-cart-info cart_preview cartpreview']//a[@class='top-cart-info-count']"));
+//
+//		//Instantiating Actions class
+//		Actions actions = new Actions(driver);
+//
+//		//Hovering on main menu
+//		actions.moveToElement(mainMenu).perform();
+		objcheckoutpage.mouseHoverOverCart();
+		for (int i = 2; i < 5; i++) {
+			String lText = excel.getStringData("ProductList", i, 1);
+			String cartVerifyXpath = "//div[@class='top-cart-content']//li//a[text()='" + lText + "']";
+			Thread.sleep(1000);
+			boolean status = driver.findElement(By.xpath(cartVerifyXpath)).isDisplayed();
+			Thread.sleep(1000);
+			booleanList.add(status);
+			Assert.assertEquals(status, true);
+		}
+
+		int flag = 0;
+		boolean firstElement = booleanList.get(0);
+		for (int i = 1; i < booleanList.size(); i++) {
+			if (!booleanList.get(i).equals(firstElement)) {
+				flag = 1;
+			}
+		}
+
+		if (flag == 0) {
+			logger.pass("All items checked out are correct !!");
+		}
 		
+		objcheckoutpage.checkoutItems(
+				excel.getStringData("BillingDetails", 0, 0),
+				excel.getStringData("BillingDetails", 0, 1),
+				excel.getStringData("BillingDetails", 0, 2),
+				excel.getStringData("BillingDetails", 0, 3),
+				excel.getNumericData("BillingDetails", 0, 4),
+				excel.getStringData("BillingDetails", 0, 5),
+				excel.getStringData("BillingDetails", 0, 6),
+				excel.getStringData("BillingDetails", 0, 7),
+				excel.getStringData("BillingDetails", 0, 8),
+				excel.getNumericData("BillingDetails", 0, 9)
+				);
+
 	}
 }
